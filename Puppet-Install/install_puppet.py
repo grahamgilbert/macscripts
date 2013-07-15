353d5caf49bc5ea55f2a01101b6134816000c77c
Graham Gilbert <graham@grahamgilbert.com>
2013-07-15 14:32:31 +0100

    Update to Puppet 3.2.2

--- Puppet-Install/install_puppet.py
+++ Puppet-Install/install_puppet.py
@@ -115,7 +115,7 @@ if internet_on:
     p.wait()
     time.sleep(20)
     print "Downloading Puppet"
-    the_dmg = downloadChunks("http://downloads.puppetlabs.com/mac/puppet-3.1.1.dmg")
+    the_dmg = downloadChunks("http://downloads.puppetlabs.com/mac/puppet-3.2.2.dmg")
     ##mount the dmg
     print "Mounting Puppet DMG"
     the_command = "/usr/bin/hdiutil attach "+the_dmg
@@ -123,12 +123,12 @@ if internet_on:
     p.wait()
     time.sleep(10)
     print "Installing Puppet"
-    the_command = "/usr/sbin/installer -pkg /Volumes/puppet-3.1.1/puppet-3.1.1.pkg -target /"
+    the_command = "/usr/sbin/installer -pkg /Volumes/puppet-3.2.2/puppet-3.2.2.pkg -target /"
     p=subprocess.Popen(the_command,shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     p.wait()
     time.sleep(20)
     print "Ejecting Puppet"
-    the_command = "hdiutil eject /Volumes/puppet-3.1.1"
+    the_command = "hdiutil eject /Volumes/puppet-3.2.2"
     subprocess.Popen(the_command,shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE).communicate()[0]
     
     print "Ejecting Facter"
