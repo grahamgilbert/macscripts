a5a7168b94ca58c1bb5e5b2650302cf7aa1a16c5
Yoann Gini <yoann.gini@gmail.com>
2017-08-04 17:40:02 +0200

    Improve script to support 10.11+ Move the version management to sub number comparaison. Change the installcheck script to avoid exception management and user proper key check

--- Munki/Auth/UnlockNetwork/installcheck.py
+++ Munki/Auth/UnlockNetwork/installcheck.py
@@ -14,38 +14,37 @@ task = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 groups = out.split('.')
 
-v = groups[0].strip() + '.' + groups[1].strip()
+v = int(groups[1].strip())
 
 command = ['/usr/bin/security', 'authorizationdb', 'read', 'system.preferences.network']
 
 task = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 (out, err) = task.communicate()
 
-formatted = plistlib.readPlistFromString(out)
+networkPreferences = plistlib.readPlistFromString(out)
 
 command = ['/usr/bin/security', 'authorizationdb', 'read', 'system.services.systemconfiguration.network']
 
 task = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 (out, err) = task.communicate()
 
-formatted2 = plistlib.readPlistFromString(out)
+systemConfig = plistlib.readPlistFromString(out)
 
 # Need to check the group for 10.9, and the rule for 10.8
 # if group matches for both rights, exit 1 as we don't need to install
-if v == '10.9' or v == '10.10':  
-    try:
+if v >= 9:
+    if 'group' in networkPreferences and 'group' in systemConfig:
         # There are two formats for this plist. WAT?
-        if formatted['group'] == group and formatted2['group'] == group:
+        if networkPreferences['group'] == group and systemConfig['group'] == group:
             sys.exit(1)
         else:
             # if it doesn't we're exiting with 0 as we need to perform the install
             sys.exit(0)
-    except:
+    else:
         # the group might not be set, let's do it
         sys.exit(0)
- 
-if v == '10.8':
-    if formatted['group'] == group and formatted2['rule'] == 'allow':
+elif v <= 8:
+    if networkPreferences['group'] == group and systemConfig['rule'] == 'allow':
         sys.exit(1)
     else:
         # if it doesn't we're exiting with 0 as we need to perform the install
