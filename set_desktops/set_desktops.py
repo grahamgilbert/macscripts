77c20ec8dd318c882aae0b5a44b2c309f69d7970
Alan Siu <alanysiu@gmail.com>
2022-03-21 21:19:17 -0700

    Allow up to three desktop wallpapers

--- set_desktops/set_desktops.py
+++ set_desktops/set_desktops.py
@@ -19,20 +19,20 @@ import sys
 parser = argparse.ArgumentParser(description='Sets the desktop picture on all screens')
 parser.add_argument('--path', help='The path of the image')
 parser.add_argument('--secondpath', help='The path of second image')
+parser.add_argument('--thirdpath', help='The path of third image')
 args = vars(parser.parse_args())
 
+picture_paths = []
+
 if args['path']:
-    picture_path = args['path']
+    picture_paths.append(args['path'])
 else:
     print >> sys.stderr, 'You must supply a path for the desktop picture'
     exit(-1)
 if args['secondpath']:
-    picture2_path = args['secondpath']
-else:
-    picture2_path = None
-
-# generate a fileURL for the desktop picture
-file_url = NSURL.fileURLWithPath_(picture_path)
+    picture_paths.append(args['secondpath'])
+if args['thirdpath']:
+    picture_paths.append(args['thirdpath'])
 
 # make image options dictionary
 # we just make an empty one because the defaults are fine
@@ -41,12 +41,23 @@ options = {}
 # get shared workspace
 ws = NSWorkspace.sharedWorkspace()
 
+# Initialize screen counter
+screen_counter = 0
+
 # iterate over all screens
-for screen in NSScreen.screens():
-    print(screen)
+while counter < len(NSScreen.screens():
+    if counter > len(picture_paths):
+        picture_counter = 0
+    else:
+        picture_counter = screen_counter
+    # generate a fileURL for the desktop picture
+    file_url = NSURL.fileURLWithPath_(picture_paths[picture_counter])
+    print(f"Setting screen {screen_counter} to {picture_paths[picture_counter]}")
     # tell the workspace to set the desktop picture
     (result, error) = ws.setDesktopImageURL_forScreen_options_error_(
-                file_url, screen, options, None)
+                file_url, NSScreen.screens[screen_counter], options, None)
     if error:
         print error
         exit(-1)
+    screen_counter += 1
+
